a607a0e94152a1726bc63130e1a271b1b7569eef
logic-three-body <1552451580@qq.com>
2020-10-05 21:56:38 +0800

    FindMax OK

---
+++ FindMax/findMax.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include<string.h>//For metset
+using namespace std;
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+
+#define MAXSIZE 20 /* 存储空间初始分配量 */
+
+const int FIND = 1;
+
+typedef int Status;/* Status是函数的类型,其值是函数结果状态代码，如OK等 */
+typedef int ElemType;/* ElemType类型根据实际情况而定，这里假设为int */
+typedef struct LNode {
+	int data;
+	struct LNode *next;
+}LNode, *LinkList;
+
+void InitList(LinkList &L)
+{
+	L = new LNode;
+	L->next = NULL;
+	return;
+}
+
+/*  随机产生n个元素的值，建立带表头结点的单链线性表L（尾插法） */
+void CreateListTail(LinkList *L, int n)
+{
+	LinkList p, r;
+	int i;
+	*L = (LinkList)malloc(sizeof(LNode)); /* L为整个线性表 */
+	r = *L;                                /* r为指向尾部的结点 */
+	for (i = 0; i < n; i++)
+	{
+		p = (LNode *)malloc(sizeof(LNode)); /*  生成新结点 */
+		std::cin >> p->data;
+		r->next = p;                        /* 将表尾终端结点的指针指向新结点 */
+		r = p;                            /* 将当前的新结点定义为表尾终端结点 */
+	}
+	r->next = nullptr;                       /* 表示当前链表结束 */
+}
+
+/* 初始条件：顺序线性表L已存在 */
+/* 操作结果：依次对L的每个数据元素输出 */
+Status ListTraverse(LinkList& L)
+{
+	LinkList p = L->next;
+	std::cout << (p->data);
+	p = p->next;
+	while (p)
+	{
+		std::cout << " ";
+		std::cout << p->data;
+		p = p->next;
+	}
+	printf("\n");
+	return OK;
+}
+
+ElemType FindMax(LinkList &L)
+{
+	int max = L->data;
+	if (!L->next)
+	{
+		return L->data;
+	}
+	max = FindMax(L->next);
+	return L->data >= max ? L->data:max;
+}
+
+int main()
+{
+	LinkList list;
+	while (true)
+	{
+		InitList(list);
+		int num = 0;
+		std::cin >> num;
+		if (!num)
+		{
+			break;
+		}
+		CreateListTail(&list, num);
+		std::cout<<FindMax(list->next)<<std::endl;		
+	}
+	return 0;
+}
