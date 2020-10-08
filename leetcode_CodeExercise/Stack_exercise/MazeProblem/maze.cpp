ffb0d84dbd120da844197b3474b30ad96ce0f2a0
logic-three-body <1552451580@qq.com>
2020-10-08 18:00:19 +0800

    MazeProblem Learning DFS

---
+++ leetcode_CodeExercise/Stack_exercise/MazeProblem/maze.cpp
@@ -0,0 +1,56 @@
+//help£ºhttps://blog.csdn.net/weixin_43722827/article/details/102755620
+#include<iostream>
+#include "maze.h"
+using namespace std;
+#define Row 100
+#define Col 3
+int End;
+int flag;
+int p[Row];
+int m[Row][Col];
+void DFS(int n);
+
+int main()
+{
+	int n;
+	while (cin >> n && n != 0)
+	{
+		flag = 0;
+		CreateMaze(n);
+		cin >> End;
+		p[1] = 1;
+		DFS(1);
+		if (flag)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+	return 0;
+}
+
+void CreateMaze(int n)
+{
+	for (int i = 1; i <= n; i++)
+		p[i] = 0;
+	for (int i = 1; i <= n; i++)
+		for (int j = 0; j < Col; j++)
+			cin >> m[i][j];
+}
+
+void DFS(int n)
+{
+	if (n == End)
+	{
+		flag = 1;
+		return;
+	}
+	for (int i = 0; i < Col; i++)
+	{
+		if (m[n][i] && !p[m[n][i]])
+		{
+			p[m[n][i]] = 1;
+			DFS(m[n][i]);
+			p[m[n][i]] = 0;
+		}
+	}
+}
